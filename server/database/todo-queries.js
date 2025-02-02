9d2a4835c2c2d1dde7e6e031b2bad7675d8a3e9d
Kendall <61364252+kmsherrin@users.noreply.github.com>
2025-02-03 09:23:48 +1100

    initial route for fetching user tasks

--- server/database/todo-queries.js
+++ server/database/todo-queries.js
@@ -1,39 +1,47 @@
 const knex = require("./connection.js");
 
-async function all() {
-    return knex('todos');
+/**
+ * Returns all TASKs for current authenticated user
+ */
+async function all(currentUser) {
+  return knex("tasks")
+    .leftJoin("users_tasks", "tasks.id", "users_tasks.task_id")
+    .where({ user_id: currentUser });
 }
 
 async function get(id) {
-    const results = await knex('todos').where({ id });
-    return results[0];
+  const results = await knex("tasks").where({ id: id });
+  return results[0];
 }
 
 async function create(title, order) {
-    const results = await knex('todos').insert({ title, order }).returning('*');
-    return results[0];
+  const results = await knex("todos").insert({ title, order }).returning("*");
+  return results[0];
 }
 
 async function update(id, properties) {
-    const results = await knex('todos').where({ id }).update({ ...properties }).returning('*');
-    return results[0];
+  const results = await knex("todos")
+    .where({ id })
+    .update({ ...properties })
+    .returning("*");
+  return results[0];
 }
 
 // delete is a reserved keyword
 async function del(id) {
-    const results = await knex('todos').where({ id }).del().returning('*');
-    return results[0];
+  const results = await knex("todos").where({ id }).del().returning("*");
+  return results[0];
 }
 
 async function clear() {
-    return knex('todos').del().returning('*');
+  return knex("todos").del().returning("*");
 }
 
 module.exports = {
-    all,
-    get,
-    create,
-    update,
-    delete: del,
-    clear
-}
+  all,
+  get,
+  create,
+  update,
+  delete: del,
+  clear,
+};
